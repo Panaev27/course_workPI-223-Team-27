218413721c809a0d9ebee25792b980777d3251ab
dyomin <wedolak@yandex.ru>
2021-05-06 21:55:30 +0500

    finding path3

--- src/main/java/calculator/PDFWriter.java
+++ src/main/java/calculator/PDFWriter.java
@@ -33,7 +33,7 @@ public class PDFWriter {
 		try {
 			//  /var/apache-tomcat-9.0.39/webapps/CreatePDF/
 			
-			PdfWriter.getInstance(document, new FileOutputStream("/tmp/Check.pdf"));
+			PdfWriter.getInstance(document, new FileOutputStream("/app/Check.pdf"));
 		} catch (FileNotFoundException | DocumentException e) {
 			e.printStackTrace();
 		}
