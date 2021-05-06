48157c1013684a43ef816979e6cce220dd392f48
dyomin <wedolak@yandex.ru>
2021-05-06 20:54:46 +0500

    Change in Calculator

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,4 +1,4 @@
-package rgr;
+package calculator;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name="Calculator", urlPatterns="/JavaCalculator")
+@WebServlet(name="Calc", urlPatterns="/JavaCalc")
 public class Calculator extends HttpServlet {
 	
 	
