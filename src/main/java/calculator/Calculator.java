cec4144af020a806ae05bcab0cf0b2a7029d2b6b
dyomin <wedolak@yandex.ru>
2021-05-06 22:16:31 +0500

    Test output

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -137,7 +137,7 @@ public class Calculator extends HttpServlet {
 				break;
 			 }
 			price = (hangar.getHangarArea()*ArrPanel[panel]*ArrType[type]+hangar.getHangarFoundationArea()*ArrFoundation[foundation]+hangar.getHangarDoorArea()*ArrDoor[door])*getTime(time)*getPromo(promo);
-			request.setAttribute("price", inputFoundation);//Double.toString(Math.floor(price*100)/100));
+			request.setAttribute("price", Double.toString(price));
 		}
 		//функция возвращает коофициент в зависимости от времени постройки
 		private double getTime(int time) {
