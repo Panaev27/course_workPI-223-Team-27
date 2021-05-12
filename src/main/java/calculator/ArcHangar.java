2aa0f60be6c6377fb1e4b24900edbd14fd5ecc1e
Panaev27 <panaev2000@mail.ru>
2021-05-12 14:53:12 +0500

    Изменение коэффициентов дверей.

--- src/main/java/calculator/ArcHangar.java
+++ src/main/java/calculator/ArcHangar.java
@@ -5,7 +5,10 @@ public class ArcHangar extends Hangar {
 	ArcHangar(double _height, double _width, double _length) {
 		super(_height, _width, _length);
 	}
-	
+	@Override
+	public double getHangarDoorArea() {
+	            return width*height*0.6;
+	}
 	@Override
 	public double getHangarArea() {
 		return Math.PI*(width/2)*((width/2)+length);
